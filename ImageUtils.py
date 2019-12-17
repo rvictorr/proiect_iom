21853722f8fdcfaacdc13b1cc0a04c1f24dba8cf
numeprenume <victrusu@gmail.com>
2019-12-17 21:03:06 +0200

    Implement image editing logic (binarization & grayscale)

---
+++ ImageUtils.py
@@ -0,0 +1,31 @@
+from PyQt5.QtGui import QImage
+import numpy as np
+
+def rgb2grayscale(img: QImage):
+    ptr = img.bits()
+    ptr.setsize(img.byteCount())
+    bytesPerPixel = img.byteCount()//(img.width()*img.height())
+    # print('original image bpp:{}'.format(bytesPerPixel))
+
+    if bytesPerPixel is 1:
+        return img
+
+    arr = np.asarray(ptr).reshape((img.height(), img.width(), bytesPerPixel))
+    mat = [0.2989, 0.5870, 0.1140, 0] if bytesPerPixel is 4 else [0.2989, 0.5870, 0.1140]
+    grayArr = np.array(np.dot(arr, mat), dtype=np.uint8)
+
+    return QImage(grayArr, grayArr.shape[1], grayArr.shape[0], QImage.Format_Grayscale8)
+
+def binarize(img: QImage, thr1, thr2):
+    img = rgb2grayscale(img)
+
+    ptr = img.bits()
+    ptr.setsize(img.byteCount())
+    bytesPerPixel = img.byteCount() // (img.width() * img.height())
+    arr = np.asarray(ptr).reshape((img.height(), img.width(), bytesPerPixel))
+    condlist = [(arr < thr1), np.logical_and(arr > thr1, arr < thr2), (arr > thr2)]
+    choicelist = [0, 85, 255]
+    bin_arr1 = np.array(np.select(condlist, choicelist), dtype=np.uint8)
+
+    return QImage(bin_arr1, bin_arr1.shape[1], bin_arr1.shape[0], QImage.Format_Grayscale8)
+
--- proiect_iom.py
+++ proiect_iom.py
@@ -1,11 +1,38 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import ImageUtils
 
 app = QApplication([])
 window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
-window.setLayout(layout)
+window.setGeometry(500, 500, 1000, 1000)
+imagesLayout = QHBoxLayout()
+window.setLayout(imagesLayout)
 window.show()
+
+image = QImage()
+image.load('test.jpg')
+# image.load('gradient.png')
+# image.load('gradient_small.png')
+# image.load('dot.png')
+
+grayscaleImg = ImageUtils.rgb2grayscale(image)
+binImg = ImageUtils.binarize(image, 70, 170)
+
+grayscaleImg.save('grayscale.jpg', None, 100)
+binImg.save('binarized.jpg', None, 100)
+
+pixMap = QPixmap(grayscaleImg)
+beforeImg = QLabel()
+beforeImg.resize(500, 500)
+beforeImg.setPixmap(pixMap.scaledToHeight(beforeImg.height()))
+imagesLayout.addWidget(beforeImg)
+
+pixMap = QPixmap(binImg)
+afterImg = QLabel()
+afterImg.resize(500, 500)
+afterImg.setPixmap(pixMap.scaledToHeight(afterImg.height()))
+imagesLayout.addWidget(afterImg)
+# window.resize(500, 500)
+
 
 app.exec()
