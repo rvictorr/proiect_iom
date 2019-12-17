7b8921b911d06f41271471693905e803c428e8fc
Radu <radu_deleanu@yahoo.com>
2019-12-17 16:52:14 +0200

    started my branch

--- proiect_iom.py
+++ proiect_iom.py
@@ -5,6 +5,7 @@ window = QWidget()
 layout = QVBoxLayout()
 layout.addWidget(QPushButton('Top'))
 layout.addWidget(QPushButton('Bottom'))
+layout.addWidget(QPushButton('Bottomz_up'))
 window.setLayout(layout)
 window.show()
 
