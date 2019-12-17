98c11add0e85f088505c016ee19ea0eccc8eaa03
Radu <radu_deleanu@yahoo.com>
2019-12-17 18:13:22 +0200

    smol edit for editor

--- proiect_iom.py
+++ proiect_iom.py
@@ -1,26 +1,41 @@
+import sys
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore
-
-'''
-app = QApplication([])
-window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
-
-window.setWindowTitle("Pro Image Edditor")
-window.setLayout(layout)
-window.show()
-'''
+from PyQt5 import QtGui, QtCore
 
 class Window(QMainWindow):
 
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 950, 500)
-        self.setWindowTitle("pRo ImAgE EdDiToR")
+        self.setWindowTitle("pRo ImAgE eDiToR")
         #self.setWindowIcon(QIcon('logo.png'))
+
+        extractAction = QtGui.QAction("&GET TO THE CHOPPAH!!!", self)
+        extractAction.setShortcut("Ctrl+Q")
+        extractAction.setStatusTip('Leave The App')
+        extractAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+
+
         self.home()
+
+
+    def home(self):
+        btn = QPushButton("Quit", self)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn.resize(60, 40)
+        btn.move(860, 440)
+        self.show()
+
+    def close_application(self):
+        print("whooaaaa so custom!!!")
+        sys.exit()
+
+
 
         '''
         # File Browser
@@ -32,14 +47,6 @@ class Window(QMainWindow):
 
         # fileMenu.addAction(file_open)
 
-    def home(self):
-        btn = QPushButton("Quit", self)
-        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        btn.resize(60, 40)
-        btn.move(860, 440)
-        self.show()
-
-
 # Function to run App
 def run():
     app = QApplication([])
@@ -47,3 +54,6 @@ def run():
     app.exec()
 
 run()
+
+
+
